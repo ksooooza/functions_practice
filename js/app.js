02b2571222ac16fac7fe964f99814d8294eadd4f
ksooooza <ksoooza@gmail.com>
2022-11-26 12:44:12 -0500

    Commit 3 - Calculate

--- js/app.js
+++ js/app.js
@@ -9,8 +9,8 @@
 
 //Commit 2 - Reverse Word Order
 
- function reverseWordOrder(phrase) {
-     return phrase.split(" ").reverse().join(" ");
+//  function reverseWordOrder(phrase) {
+//      return phrase.split(" ").reverse().join(" ");
     //OR use this method
     // let words = [];
     // let currentWord = '';
@@ -24,8 +24,27 @@
     //         if(i + 1 === phrase.length) words.push(currentWord);
     //     }
     // } return words;
-}
+//}
 
-console.log(reverseWordOrder("Ishmael me Call"));
-console.log(reverseWordOrder("I use Lâncome on my comb"));
+// console.log(reverseWordOrder("Ishmael me Call"));
+// console.log(reverseWordOrder("I use Lâncome on my comb"));
 
+//Commit 3 - Calculate
+
+function calculate(num1, num2, operation) {
+    if (operation == "add") {
+        return num1 + num2;
+    } else if (operation == "sub") {
+        return num1 - num2;
+    } else if (operation == "mult") {
+        return num1 * num2;
+        } else if (operation == "div") {
+        return num1 / num2;
+    } else {
+        if (operation == "exp")
+        return num1**num2;
+     }
+} 
+
+console.log(calculate(4, 3, "sub"));
+console.log(calculate(4, 3, "exp"));
