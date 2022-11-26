83d33c9e24768eda285472c20f67ecce8cdddb05
ksooooza <ksoooza@gmail.com>
2022-11-26 12:18:48 -0500

    Commit 2 - reverseWordOrder

--- js/app.js
+++ js/app.js
@@ -1,5 +1,31 @@
-function printGreeting(name) {
-    return `Hello there ${name}`;
-} 
+//Commit 1 - Print Greeting
 
-console.log(printGreeting("Slimer"));
+// function printGreeting(name) {
+//     return `Hello there ${name}`;
+// } 
+
+//console.log(printGreeting("Slimer"));
+
+
+//Commit 2 - Reverse Word Order
+
+ function reverseWordOrder(phrase) {
+     return phrase.split(" ").reverse().join(" ");
+    //OR use this method
+    // let words = [];
+    // let currentWord = '';
+    // for (let i = 0; i <= phrase.length; i++) {
+    //     if (phrase[i] === " ") {
+    //     words.push(currentWord);
+    //     //words.splice(words.length -1, 0, currentWord);
+    //     currentWord = '';
+    //     } else {
+    //         currentWord += phrase[i];
+    //         if(i + 1 === phrase.length) words.push(currentWord);
+    //     }
+    // } return words;
+}
+
+console.log(reverseWordOrder("Ishmael me Call"));
+console.log(reverseWordOrder("I use Lâncome on my comb"));
+
